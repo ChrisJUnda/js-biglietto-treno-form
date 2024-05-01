35e48002722c50d97bd3767c4e8f3eff215f879c
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-01 14:38:09 +0200

    [Ex Done]

---
+++ js.main/main.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// console.log('Buongiorno Gaetano che piacere vederti!');
+
+//bottone genera ticket
+const generateButton = document.getElementById('generate-ticket');
+const resetButton = document.getElementById('reset-ticket');
+
+const generateTicketForm = document.getElementById('generate-ticket-form');
+
+generateTicketForm.addEventListener('submit', function (event) {
+  console.log('ho cliccato sul bottone genera');
+
+  event.preventDefault();
+
+  //html elements
+  const ticketResultElement = document.getElementById('ticket-result');
+
+  //input utente
+  const userNameInput = document.getElementById('user-name-input');
+  const ageInput = document.getElementById('age-input');
+  const kmInput = document.getElementById('km-input');
+
+  //output biglietto
+
+  const userNameOutput = document.getElementById('user-name-output');
+  const offerOutput = document.getElementById('offer-output');
+  const carOutput = document.getElementById('car-output');
+  const codeOutput = document.getElementById('code-output');
+  const priceOutput = document.getElementById('price-output');
+
+  console.dir(userNameInput);
+  console.dir(userNameOutput);
+  console.dir(offerOutput);
+  console.dir(carOutput);
+  console.dir(codeOutput);
+  console.dir(priceOutput);
+
+  //dati per il calcolo del biglietto
+
+  const kmPrice = 0.21;
+  const discountUnder = 20;
+  const discountOver = 40;
+
+  let price = kmPrice * Number(kmInput.value);
+  let offerDescription = 'Offerta Standard';
+
+  if (ageInput.value === 'under') {
+
+    price -= (price / 100) * discountUnder;
+    offerDescription = 'Offerta Under 18';
+  } else if (ageInput.value === 'over') {
+    price -= -(price / 100) * discountOver;
+    offerDescription = 'Offerta Over 65';
+  }
+
+  //mostro area del biglietto
+
+
+  ticketResultElement.classList.remove('hidden');
+
+  //popolo campi biglietto
+
+  userNameOutput.innerText = userNameInput.value;
+  offerOutput.innerText = offerDescription;
+  priceOutput.innerText = `${price.toFixed(2)} €`;
+});
+
+resetButton.addEventListener('click', function () {
+  const ticketResultElement = document.getElementById('ticket-result');
+
+  ticketResultElement.classList.add('hidden');
+});
